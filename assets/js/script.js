31f4115372e85c54debfb4d11b210ecf3b4de212
JuanDuran85 <duran85juan@gmail.com>
2020-11-03 12:14:53 -0300

    agregando solucion a requerimiento 1 y 2

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var dental = [
+    {hora: "8:30", especialista: "ANDREA ZUÑIGA", paciente: "MARCELA RETAMAL", rut: "11123425-6", prevision: "ISAPRE"},
+    {hora: "11:00", especialista: "MARIA PIA ZAÑARTU", paciente: "ANGEL MUÑOZ", rut: "9878789-2", prevision: "ISAPRE"},
+    {hora: "11:30", especialista: "SCARLETT WITTING", paciente: "MARIO KAST", rut: "7998789-5", prevision: "FONASA"},
+    {hora: "13:00", especialista: "FRANCISCO VON TEUBER", paciente: "KARIN FERNANDEZ", rut: "18887662-K", prevision: "FONASA"},
+    {hora: "13:30", especialista: "EDUARDO VIÑUELA", paciente: "HUGO SANCHEZ", rut: "17665461-4", prevision: "FONASA"},
+    {hora: "14:00", especialista: "RAQUEL VILLASECA", paciente: "ANA SEPULVEDA", rut: "14441281-0", prevision: "ISAPRE"},
+];
+
+var radiologia = [
+    { hora: '11:00', especialista: 'IGNACIO SCHULZ', paciente: 'FRANCISCA ROJAS', rut: '9878782-1', prevision: 'FONASA' },
+    { hora: '11:30', especialista: 'FEDERICO SUBERCASEAUX', paciente: 'PAMELA ESTRADA', rut: '15345241-3', prevision: 'ISAPRE' },
+    { hora: '15:00', especialista: 'FERNANDO WURTHZ', paciente: 'ARMANDO LUNA', rut: '16445345-9', prevision: 'ISAPRE' },
+    { hora: '15:30', especialista: 'ANA MARIA GODOY', paciente: 'MANUEL GODOY', rut: '17666419-0', prevision: 'FONASA' },
+    { hora: '16:00', especialista: 'PATRICIA SUAZO', paciente: 'RAMON ULLOA', rut: '14989389-K', prevision: 'FONASA' },
+];
+
+var traumatologia = [
+    { hora: '8:00', especialista: 'MARIA PAZ ALTUZARRA', paciente: 'PAULA SANCHEZ', rut: '15554774-5', prevision: 'FONASA' },
+    { hora: '09:00', especialista: 'RENE POBLETE', paciente: 'ANA GELLONA', rut: '13123329-', prevision: 'ISAPRE' },
+    { hora: '10:00', especialista: 'RAUL ARAYA', paciente: 'ANGÉLICA NAVAS', rut: '15444147-9', prevision: 'ISAPRE' },
+    { hora: '10:30', especialista: 'MARIA ARRIAGADA', paciente: 'ANA KLAPP', rut: '17879423-9', prevision: 'ISAPRE' },
+    { hora: '11:00', especialista: 'ALEJANDRO BADILLA', paciente: 'FELIPE MARDONES', rut: '1547423-6', prevision: 'ISAPRE' },
+    { hora: '12:00', especialista: 'ARTURO CAVAGNARO', paciente: 'CECILIA MENDEZ', rut: '9747535-8', prevision: 'ISAPRE' },
+    { hora: '12:30', especialista: 'ANDRES KANACRI', paciente: 'MARCIAL SUAZO', rut: '11254785-5', prevision: 'ISAPRE' },
+];
+
+
+// mostrando primera y ultima atencion de paciente de cada tabla, nombre, prevision (Primera atención: Juan Pérez - Fonasa | Última atención: Ana Gálvez - Isapre.)
+
+document.write("<div class='container bg-light'>");
+
+document.write("<h3>Atenciones dentales</h3>");
+document.write(`<p><i class="fas fa-arrow-right"></i> Primero atención: ${dental[0].paciente} - ${dental[0].prevision} | Última antención: ${dental[dental.length - 1].paciente} - ${dental[dental.length - 1].prevision}</p>`);
+
+document.write("<h3>Atenciones radiologia</h3>");
+document.write(`<p><i class="fas fa-arrow-right"></i> Primero atención: ${radiologia[0].paciente} - ${radiologia[0].prevision} | Última antención: ${radiologia[radiologia.length - 1].paciente} - ${radiologia[radiologia.length - 1].prevision}</p>`);
+
+document.write("<h3>Atenciones traumatologia</h3>");
+document.write(`<p><i class="fas fa-arrow-right"></i> Primero atención: ${traumatologia[0].paciente} - ${traumatologia[0].prevision} | Última antención: ${traumatologia[traumatologia.length - 1].paciente} - ${traumatologia[traumatologia.length - 1].prevision}</p>`);
+
+document.write("</div>");
